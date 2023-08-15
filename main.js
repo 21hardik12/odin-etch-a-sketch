7f0892724ff560e2bca9f9b3823e594f08f33de9
21hardik12 <21hardik12@gmail.com>
2023-08-15 09:35:47 +0530

    finish all the toggle button functions

--- main.js
+++ main.js
@@ -20,8 +20,12 @@ penColorPicker.addEventListener('input', event => {
 const toggleButtons = document.querySelectorAll('.toggle');
 toggleButtons.forEach(button => {
     button.addEventListener('click', e => {
-        document.querySelectorAll('.toggle').forEach(b => b.classList.remove('button-enabled'));
-        button.classList.add('button-enabled');
+        if (button.classList.contains('button-enabled')) {
+            button.classList.remove('button-enabled');
+        } else {
+            document.querySelectorAll('.toggle').forEach(b => b.classList.remove('button-enabled'));
+            button.classList.toggle('button-enabled');
+        }
     });
 });
 
@@ -48,6 +52,21 @@ bgColorPicker.addEventListener('input', event => {
     });
 });
 
+function adjustColorShade(hexColor, adjustment) {
+    const r = parseInt(hexColor.slice(1, 3), 16);
+    const g = parseInt(hexColor.slice(3, 5), 16);
+    const b = parseInt(hexColor.slice(5, 7), 16);
+
+    const newR = Math.max(0, Math.min(255, r + adjustment));
+    const newG = Math.max(0, Math.min(255, g + adjustment));
+    const newB = Math.max(0, Math.min(255, b + adjustment));
+
+    const newHexColor = `#${newR.toString(16).padStart(2, '0')}${newG.toString(16).padStart(2, '0')}${newB.toString(16).padStart(2, '0')}`;
+    
+    return newHexColor;
+}
+
+
 function draw() {
     const gridContainer = document.querySelector('.grid-container');
 
@@ -57,11 +76,28 @@ function draw() {
     gridContainer.addEventListener('mousemove', (e) => {
         if (isDrawing) {
             if (e.target.classList.contains('grid-element')) {
-                if (!document.getElementById('eraser').classList.contains('button-enabled')) {
-                    e.target.style.backgroundColor = penColor;
-                } else {
+                if (document.getElementById('eraser').classList.contains('button-enabled')) {
                     e.target.style.backgroundColor = bgColor;
+                    return;
                 }
+                
+                if (document.getElementById('rainbow').classList.contains('button-enabled')) {
+                    e.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                    return;
+                }
+
+                if (document.getElementById('shading').classList.contains('button-enabled')) {                   
+                    e.target.style.backgroundColor = adjustColorShade(rgbToHex(e.target.style.backgroundColor), -10);
+                    return;
+                }
+                
+                if (document.getElementById('lighten').classList.contains('button-enabled')) {
+                    console.log("lighten enabled");
+                    e.target.style.backgroundColor = adjustColorShade(rgbToHex(e.target.style.backgroundColor), 10);
+                    return;
+                }
+
+                e.target.style.backgroundColor = penColor;
             }
         }
     });
